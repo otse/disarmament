72427e52a8bc358a139ee17cc67626927e3e1b40
Okay <1123362+otse@users.noreply.github.com>
2024-11-30 11:31:57 +0100

    mycomponent...
    game components have to follow the boot, loop and clear idiom used by the client
    for now only tunnels is a real component
    component.js is empty because an interface is type information

---
+++ js/lib/component.js
@@ -0,0 +1,1 @@
+/// Game Components have to follow the boot, loop and clear idiom used by the client
---
+++ src/lib/component.ts
@@ -0,0 +1,8 @@
+/// Game Components have to follow the boot, loop and clear idiom used by the client
+
+interface mycomponent {
+    componentName: string;
+    boot: () => void;
+    loop: () => void;
+    clear: () => void;
+}
